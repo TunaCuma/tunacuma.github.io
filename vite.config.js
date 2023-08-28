68f92dd736e8b1ca488e2e22e7a36723321273f5
Tuna Cuma <tunacuma2003@gmail.com>
2023-08-28 17:51:25 +0300

    deneme

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
     };
 
     if (command !== "serve") {
-        config.base = "/viteproje/";
+        config.base = "/";
     }
 
     return config;
