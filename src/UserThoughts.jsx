3bc9e460b7e945dedec82f99033fbf735a5b519f
Tuna Cuma <tunacuma2003@gmail.com>
2023-09-02 16:48:27 +0300

    responsive v4

--- src/UserThoughts.jsx
+++ src/UserThoughts.jsx
@@ -10,6 +10,37 @@ const MAX_VISIBILITY = 5;
 export default function UserThoughts({ children }) {
     const [active, setActive] = useState(4);
     const count = React.Children.count(children);
+    const [touchStart, setTouchStart] = useState(null);
+    const [touchEnd, setTouchEnd] = useState(null);
+
+    // the required distance between touchStart and touchEnd to be detected as a swipe
+    const minSwipeDistance = 50;
+
+    const onTouchStart = (e) => {
+        setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+        setTouchStart(e.targetTouches[0].clientX);
+    };
+
+    const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+    const onTouchEnd = () => {
+        if (!touchStart || !touchEnd) return;
+        const distance = touchStart - touchEnd;
+        const isLeftSwipe = distance > minSwipeDistance;
+        const isRightSwipe = distance < -minSwipeDistance;
+        if (isRightSwipe) {
+            if (active > 0) {
+                setActive((i) => i - 1);
+            } else {
+                setActive(count - 1);
+            }
+        } else if (isLeftSwipe) {
+            if (active < count - 1) {
+                setActive((i) => i + 1);
+            }
+        }
+        // add your conditional logic here
+    };
 
     return (
         <div className="UserThoughts">
@@ -29,6 +60,9 @@ export default function UserThoughts({ children }) {
                                     ? "none"
                                     : "block",
                         }}
+                        onTouchStart={onTouchStart}
+                        onTouchMove={onTouchMove}
+                        onTouchEnd={onTouchEnd}
                     >
                         {child}
                     </div>
